b63f10536b530972afff037b6f415a01f7a8d5e2
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2011-09-08 11:18:42 +0100

    Turned site name into a link back to the homepage.

--- datastage/web/core/urls.py
+++ datastage/web/core/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import patterns, include, url
 from datastage.web.core import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.SimpleView.as_view(template_name='core/index'))
+    url(r'^$', views.SimpleView.as_view(template_name='core/index'), {}, 'index'),
 )
