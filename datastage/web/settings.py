e0b0beaf7de40cca18fd0b27720a42a97bb2a035
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-01-16 12:40:18 +0000

    Added login/logout links and fixed LOGIN_REDIRECT_URL.

--- datastage/web/settings.py
+++ datastage/web/settings.py
@@ -142,6 +142,7 @@ INSTALLED_APPS = (
 
 LOGIN_URL = '/login/'
 LOGOUT_URL = '/logout/'
+LOGIN_REDIRECT_URL = '/'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
