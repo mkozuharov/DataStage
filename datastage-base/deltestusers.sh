91a0aba8b2cc69082d736f3b59961ca9d6eba416
bhavana <bhavana.kaushik@gmail.com>
2012-01-25 13:16:35 +0000

    Removed unused admiral-base scripts Also modified/removed ldap references from some scripts.

--- datastage-base/deltestusers.sh
+++
@@ -1,6 +0,0 @@
-./admiraluserdel.sh TestUser1 purge
-./admiraluserdel.sh TestUser2 purge
-./admiraluserdel.sh TestLeader purge
-./admiraluserdel.sh TestCollab purge
-./admiraluserdel.sh TestUserD purge
-
