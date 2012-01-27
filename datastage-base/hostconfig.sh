e4601cfeb3d0a405f1c48afee4bd6a1648758679
bhavana <bhavana.kaushik@gmail.com>
2012-01-27 11:57:26 +0000

    added hostconfig file

---
+++ datastage-base/hostconfig.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Configuration parameters for host Zakynthos
+#
+# (See also: admiralconfig.sh)
+#
+
+RESEARCHGROUPNAME="Datastage"
+
+ADMIRALDOMAINDC="zoo ox ac uk"
+ADMIRALHOSTNAME="zakynthos"
+
+# Samba/Windows workgroup name for research group files
+WORKGROUP="ExampleResearchGroup"
+
+
+# End.
+
+
+    
