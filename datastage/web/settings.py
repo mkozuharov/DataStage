ee64fc027e2c52cb2b898fb35713f67ffd08e29b
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-01-31 17:44:43 +0000

    Getting database working with a password, as our server no longer runs as datastage.

--- datastage/web/settings.py
+++ datastage/web/settings.py
@@ -22,6 +22,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'datastage',                      # Or path to database file if using sqlite3.
+        'HOST': 'localhost',
+        'USER': 'datastage',
+        'PASSWORD': open(settings['server:database_password_file'], 'r').read().strip(),
     }
 }
 
