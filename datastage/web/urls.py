71541910976b7dd28ecc2b2d7ba5a3e54313be5c
bhavana <bhavana.kaushik@gmail.com>
2012-02-27 12:01:24 +0000

    changed data to dastage for http access

--- datastage/web/urls.py
+++ datastage/web/urls.py
@@ -36,7 +36,7 @@ admin.autodiscover()
 import django.contrib.auth.views as auth_views
 
 urlpatterns = patterns('',
-    url(r'^data/', include('datastage.web.browse.urls', 'browse')),
+    url(r'^datastage/', include('datastage.web.browse.urls', 'browse')),
     url(r'^docs/', include('datastage.web.documentation.urls', 'documentation')),
     url(r'^dataset/', include('datastage.web.dataset.urls', 'dataset')),
     url(r'^', include('datastage.web.core.urls', 'core')),
