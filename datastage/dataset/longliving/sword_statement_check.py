8dd28c3ed737d90f49c74922b94bc994bda6ac2d
Richard <richard@magic-box.(none)>
2012-02-27 12:22:48 +0000

    add error handling and throttling capacity to statement checker

--- datastage/dataset/longliving/sword_statement_check.py
+++ datastage/dataset/longliving/sword_statement_check.py
@@ -3,6 +3,7 @@ import time
 import thread
 import urllib2
 import sys
+import datetime
 
 from django_longliving.base import LonglivingThread
 
@@ -29,26 +30,54 @@ class SwordStatementCheckThread(LonglivingThread):
     # FIXME: not quite sure how the __init__ function on LonglivingThread, 
     # so setting this as a class variable for the time being
     
-    # this is how long the thread will sleep between requests
+    # this is how long the thread will sleep between requests (in seconds)
     throttle = 5
+    
+    # this is how long the thread will sleep between retrys (in seconds)
+    retry_delay = 3
+    
+    # This is how long the thread will sleep between entire batches of updates.
+    # This is particularly useful if the total number of submissions is quite
+    # small - it will stop the while True loop just spinning aimlessly most of
+    # the time. (in seconds)
+    batch_throttle = 120
     
     # this is how many times the thread will re-try contacting the server if
     # it suffers a major exception (i.e. not a sword exception, but something
     # network related)
     retry_count = 10
     
+    # this is the gap between attempts to check a specific item.  If the item
+    # has been checked more recently than this amount of time ago, it will not
+    # be checked again on the current run.  Specified in seconds (here it is
+    # set to once per day).
+    check_gap = 86400
+    
     def run(self):
         # just keep going until the thread is killed
         while True:
             self._check_all_datasets()
+            time.sleep(SwordStatementCheckThread.batch_throttle)
     
     def _check_all_datasets(self):
         dss = DatasetSubmission.objects.all()
         for dataset_submission in dss:
+            if not self._checkable(dataset_submission):
+                continue
             self._check_dataset(dataset_submission)
+
+    def _checkable(self, dataset_submission):
+        last_checked = dataset_submission.last_checked
+        if last_checked is None:
+            return True
+        now = datetime.datetime.now()
+        minimum = datetime.timedelta(0, SwordStatementCheckThread.check_gap)
+        gap = now - last_checked
+        return gap > minimum
 
     def _check_dataset(self, dataset_submission):
         retry_counter = 0
+        exception = None
         while retry_counter < SwordStatementCheckThread.retry_count:
             try:
                 logger.info("Checking state of dataset at " + dataset_submission.remote_url)
@@ -62,56 +91,25 @@ class SwordStatementCheckThread(LonglivingThread):
                     logger.info("Dataset has state URI: " + state_uri)
                     if state_uri in ERROR_STATES:
                         dataset_submission.status = 'error'
-                        dataset_submission.save()
                         logger.info("URI: " + state_uri + " is an error state ... setting 'error' state on submission record")
+                        break
+                dataset_submission.last_checked = datetime.datetime.now()
+                dataset_submission.save()
                 time.sleep(SwordStatementCheckThread.throttle)
         
             except urllib2.URLError as e:
                 # if we get an exception, try again up to the limit
+                logger.info("Got error connecting to the server ... retrying " + str(retry_counter + 1) + " of " + str(SwordStatementCheckThread.retry_count))
                 retry_counter += 1
+                exception = e
+                time.sleep(SwordStatementCheckThread.retry_delay)
                 continue
                 
             else:
                 # if we don't get an exception, we're done
                 return
-            
-            
-    """
-    def run(self):
-        client = self.get_redis_client()
         
-        for key, pk in self.watch_queue(client, SUBMISSION_QUEUE, True):
-            try:
-                self.process_item(client, pk)
-            except Exception:
-                logger.exception("Failed to process submission")
-                try:
-                    dataset_submission = DatasetSubmission.objects.get(pk=pk)
-                    dataset_submission.status = 'error'
-                    dataset_submission.save()
-                except Exception:
-                    logger.exception("Failed to mark submission as failed")
-    """             
-    
-    """
-    def process_item(self, client, pk):
-        dataset_submission = DatasetSubmission.objects.get(pk=pk)
+        # if we don't return from the else statement above, it means the retries
+        # all failed, and we have a problem.  Raise the last thrown exception.
+        raise exception
         
-        logger.info("Received submission request for %r to %r",
-                    dataset_submission.identifier,
-                    dataset_submission.repository.homepage)
-        
-        dataset = dataset_submission.dataset
-        opener = openers.get_opener(dataset_submission.repository,
-                                    dataset_submission.submitting_user)
-        
-        def update_status(status):
-            logger.debug("Status updated to %r", status)
-            dataset_submission.status = status
-            dataset_submission.save()
-
-        dataset.complete_submission(opener, dataset_submission, update_status)
-
-        logger.info("Submission completed")
-    """
-    
