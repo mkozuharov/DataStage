c38de028829f84e981d750760a17806f1f257d47
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-05-11 16:44:30 +0100

    Fixed DATAFLOW-321 (title and desc fields not tabbable-to.).
    JS now no longer replaces these input fields with spans. Instead, we attach
    event handlers to the fields to POST the update when they are changed
    and then blurred.

--- datastage/web/static/base.js
+++ datastage/web/static/base.js
@@ -24,50 +24,22 @@
 # -------------------------------------------------------------------- */
 
 $(function() {
-  $('.meta-title').each(blurTitle);
-  $('.meta-description').each(blurDescription);
+  $('.meta-title, .meta-description').change(metaChanged).blur(updateMeta);
   $('.meta-submit').remove();
-  //$('.meta-title-header').text($('.meta-title-header').text() + ' (click to edit)');
 });
 
-function editTitle() {
-  var span = $(this);
-  var input = $('<input/>').val(span.text())
-                           .attr('name', span.attr('name'))
-                           .attr('class', span.attr('class'));
-  span.replaceWith(input);
-  input.focus().blur(updateMeta(input, blurTitle));
+function metaChanged() {
+	$(this).addClass("meta-changed");
 }
 
-function editDescription() {
-  var span = $(this);
-  var input = $('<textarea/>').text(span.text())
-                           .attr('name', span.attr('name'))
-                           .attr('class', span.attr('class'));
-  span.replaceWith(input);
-  input.focus().blur(updateMeta(input, blurDescription));
+function updateMeta() {
+	var field = $(this);
+	if (field.hasClass("meta-changed")) {
+		var postData = {};
+		postData[field.attr('name')] = field.val();
+		postData['csrfmiddlewaretoken'] = $('input[name$="csrfmiddlewaretoken"]').val();
+		$.post('', postData);
+		field.removeClass("meta-changed");
+	}
 }
 
-function updateMeta(field, blurFunc) { return function() {
-  postData = {};
-  postData[field.attr('name')] = field.val();
-  postData['csrfmiddlewaretoken'] = $('input[name$="csrfmiddlewaretoken"]').val();
-  $.post('', postData);
-  blurFunc(null, field);
-}; }
-
-function blurTitle(i, input) {
-  input = $(input);
-  input.replaceWith($('<span/>').text(input.val())
-                                .attr('name', input.attr('name'))
-                                .attr('class', input.attr('class'))
-                                .click(editTitle));
-}
-
-function blurDescription(i, input) {
-  input = $(input);
-  input.replaceWith($('<span/>').text(input.val())
-                                .attr('name', input.attr('name'))
-                                .attr('class', input.attr('class'))
-                                .click(editDescription));
-}
