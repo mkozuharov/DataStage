b8f7a14b6b4fa9db608f727035cbfca39eb3d0f3
Mihail Kozhuharov <mkozuharov@gmail.com>
2013-09-14 14:14:49 +0300

    Fixed an issue with removing the os user from os groups when deleting. Added comments.

--- datastage/admin/users.py
+++ datastage/admin/users.py
@@ -30,7 +30,6 @@ import random
 import re
 import string
 import subprocess
-import shutil
 import libmount
 import getpass
 
@@ -39,10 +38,20 @@ from django.contrib.auth.models import User
 from datastage.config import settings
 from .menu_util import menu, ExitMenu
 from .sync_permissions import sync_permissions
-from .projects import delete_user_from_project, purge_user_from_project
+from datastage.admin.projects import purge_user_from_project, delete_user_from_project
 
 
 def users_menu():
+    """Main configuration menu for users
+
+    Prints a list with all datastage users.
+    Available options are:
+    - creating a new user on the system and adding it to datastage
+    - adding an user to datastage, which already exists on the system
+    - changing user password - this changes both system and samba password
+    - deleting a user from datastage
+
+    """
     while True:
         print "======================="
         print "List of Datatsage users"
@@ -69,6 +78,12 @@ def users_menu():
 
 
 def add_new_user():
+    """Configuration screen for adding a new user.
+
+    Prompts for user details - username, names, email.
+    After that delegates the actual user creation to create_user
+
+    """
     username, first_name, last_name, email = None, None, None, None
     print "================================="
     print "Add user (press Ctrl-D to cancel)"
@@ -112,6 +127,12 @@ def add_new_user():
 
 
 def create_user(username, first_name, last_name, email):
+    """Creates a new user on the system and adds it to datastage.
+
+    Checks for username conflicts, creates the database objects and sets the password to a random string.
+    The user is not added to any os or datastage groups, because it is not assigned to a project yet.
+
+    """
     try:
         pwuser = pwd.getpwnam(username)
         if pwuser:
@@ -151,6 +172,11 @@ def create_user(username, first_name, last_name, email):
 
 
 def add_existing_user():
+    """Configuration screen for adding an existing os user to datastage.
+
+    Prompts for user infirmation - username, names and email and calls create_existing_user
+
+    """
     username, first_name, last_name, email = None, None, None, None
     print "================================="
     print "Add user (press Ctrl-D to cancel)"
@@ -191,6 +217,14 @@ def add_existing_user():
 
 
 def create_existing_user(username, first_name, last_name, email):
+    """Registers an existing os user with datastage.
+
+    Checks for username conflicts and creates the database objects. The existing user password is not changed.
+    If the /etc/passwd comment field is empty, it is set with the full user name.
+    The user is not added to any os or datastage groups, because it is not assigned to a project yet.
+    The user has to be registered with a samba password manually, because we do not know the actual user password.
+
+    """
     user, created = User.objects.get_or_create(username=username)
     if not created:
         print "Error: Datastage user with such username already exists!"
@@ -213,6 +247,11 @@ def create_existing_user(username, first_name, last_name, email):
 
 
 def change_passwd():
+    """Configuration screen for changing the password of a user.
+
+    Changes both system and samba passwords.
+
+    """
     print "========================================"
     print "Change Password (press Ctrl-D to cancel)"
     print "========================================"
@@ -229,6 +268,11 @@ def change_passwd():
 
 
 def remove_user():
+    """Configuration screen for removing a user from datastage,
+
+    The user can be deleted while retaining its data, or the data can be deleted along with the user.
+
+    """
     username = None
     print "===================================="
     print "Remove user (press Ctrl-D to cancel)"
@@ -252,7 +296,12 @@ def remove_user():
 
 
 def purge_user(username):
-    user, groups, projects = None, None, []
+    """Delete a user from datastage including all user data in all projects in which the user participates.
+
+    The os user can be retained or deleted.
+
+    """
+    user, groups = None, None
     try:
         user = User.objects.get(username=username)
     except User.DoesNotExist:
@@ -261,16 +310,14 @@ def purge_user(username):
 
     groups = user.groups.all()
     for group in groups:
-        #populate list with all projects in which the user participates
+        #remove the user from all projects, in which it participates
         if group.name.endswith('leader'):
-            projects = group.leaders_of_project
+            project = group.leaders_of_project
         elif group.name.endswith('member'):
-            projects = group.members_of_project
+            project = group.members_of_project
         elif group.name.endswith('collab'):
-            projects = group.collaborators_of_project
-
-        for project in projects:
-            purge_user_from_project(user, project, group, False)
+            project = group.collaborators_of_project
+        yield purge_user_from_project(user, project, group, False)
 
     #delete the user object from the database
     user.delete()
@@ -296,6 +343,12 @@ def purge_user(username):
 
 
 def delete_user(username):
+    """Delete a user from datastage while preserving all user data.
+
+    The user data is orphaned and the ownership is transferred to the datastage orphan user.
+    The os user can be retained or deleted.
+
+    """
     user, groups, projects = None, None, []
     datastage_orphan = pwd.getpwnam(settings.get('main:datastage_orphan'))
     try:
@@ -306,16 +359,14 @@ def delete_user(username):
 
     groups = user.groups.all()
     for group in groups:
-        #populate list with all projects in which the user participates
+        #remove the user from all projects, in which it participates
         if group.name.endswith('leader'):
-            projects = group.leaders_of_project
+            project = group.leaders_of_project
         elif group.name.endswith('member'):
-            projects = group.members_of_project
+            project = group.members_of_project
         elif group.name.endswith('collab'):
-            projects = group.collaborators_of_project
-
-        for project in projects:
-            delete_user_from_project(user, project, group, False)
+            project = group.collaborators_of_project
+        yield delete_user_from_project(user, project, group, False)
 
     #delete the user object from the database
     user.delete()
